36b5142ed470d2080dbfc4110acaef3b4eaaa063
Gideon Providence <jprovidence@live.ca>
2011-11-19 04:00:30 -0500

    clean up

--- concrete/Debug/src/concrete.d
+++
@@ -1,6 +0,0 @@
-src/concrete.d src/concrete.o: ../src/concrete.c ../src/concrete.h \
- ../src/ladder.h
-
-../src/concrete.h:
-
-../src/ladder.h:
--- concrete/Debug/src/ladder.d
+++
@@ -1,3 +0,0 @@
-src/ladder.d src/ladder.o: ../src/ladder.c ../src/ladder.h
-
-../src/ladder.h:
